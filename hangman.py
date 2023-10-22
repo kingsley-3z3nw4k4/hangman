6913f35de6a8c3927f182b78bcf9b0472c3dd3c4
unknown <k3z3nw4k4@gmail.com>
2023-10-22 02:50:58 +0100

    My first commit

---
+++ hangman.py
@@ -0,0 +1,30 @@
+def hangman():
+    stage = 0
+    wrong_guesses = ["","_______   ","|    |   ","|    0   ","|   /|\  ","|   / \  ","|        "]
+    word = "goat"
+    score_board = ['_']*len(word)
+    win = False
+
+    print("Welcome to Hang Man")
+
+    while stage < len(wrong_guesses)-1:
+        print('\n')
+        guess = input('Guess a letter:... ')
+
+        if guess in word:
+            score_board[word.index(guess)] = guess
+        else:
+            stage += 1
+
+        print(' '.join(score_board))
+        print('\n'.join(wrong_guesses[0:stage+1]))
+
+        if '_' not in score_board:
+            print(f"\nYou win! The word was: {''.join(score_board)}")
+            win = True
+            break
+
+    if not win:
+        print('\nYou lose!')
+
+hangman()
